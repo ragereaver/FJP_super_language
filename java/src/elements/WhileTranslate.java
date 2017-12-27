c8802fff80bfe7888144273385b3ce72c8bba420
Michal Tusl <m.tusl@seznam.cz>
2017-12-27 15:43:25 +0100

    if, while (bez body)

--- java/src/elements/WhileTranslate.java
+++ java/src/elements/WhileTranslate.java
@@ -1,7 +1,10 @@
 package elements;
 
+import enums.EInstructionSet;
+import generatedParser.SLLanguageMainListener;
 import generatedParser.SLLanguageParser;
 import org.antlr.v4.runtime.tree.ParseTree;
+import tableClasses.TableOfCodes;
 
 /**
  * Created by BobrZlosyn on 22.12.2017.
@@ -10,11 +13,17 @@ public class WhileTranslate extends IfTranslate{
 
     public void runWhile(SLLanguageParser.CycleContext ctx) {
 
+
         doCondition(ctx.expression(), ctx.getStart()); // teoreticky by to melo byt stejne
+
         doBody(ctx.compoundStatement(0));
+        //momentalně se body překládá po while
+        EInstructionSet.doInstruction(EInstructionSet.JUMP_COMP, 23); //přepsat adresu na dalsi instrukci po while
+        EInstructionSet.doInstruction(EInstructionSet.JUMP, 18); //přepsat adresu na začátek while
     }
 
     public void doBody(ParseTree body) {
+
         //TODO: telo cyklu
     }
 }
